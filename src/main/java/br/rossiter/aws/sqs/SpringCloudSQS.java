2525683ce0eabf53bd29a5a3df0859f2148afd14
rrossiter <rrossiter@gmail.com>
2023-04-03 23:29:29 -0300

    initial commit

--- src/main/java/br/rossiter/aws/sqs/SpringCloudSQS.java
+++ src/main/java/br/rossiter/aws/sqs/SpringCloudSQS.java
@@ -2,7 +2,6 @@ package br.rossiter.aws.sqs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.context.annotation.Lazy;
@@ -17,8 +16,23 @@ public class SpringCloudSQS {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringCloudSQS.class);
 
-    @SqsListener(value = "spring-cloud-test-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @SqsListener(value = "queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receiveMessage(String message, @Headers Map<String, String> headers) {
+        logger.info("Received message: {}, having SenderId: {}", message, headers);
+    }
+
+    @SqsListener(value = "queue1", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receiveMessage1(String message, @Headers Map<String, String> headers) {
+        logger.info("Received message: {}, having SenderId: {}", message, headers);
+    }
+
+    @SqsListener(value = "queue2", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receiveMessage2(String message, @Headers Map<String, String> headers) {
+        logger.info("Received message: {}, having SenderId: {}", message, headers);
+    }
+
+    @SqsListener(value = "spring-cloud-test-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receiveMessage3(String message, @Headers Map<String, String> headers) {
         logger.info("Received message: {}, having SenderId: {}", message, headers);
     }
 
